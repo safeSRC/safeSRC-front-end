efbd551cbd8a19023eef35e6bd32ca2b984d7aed
Brianna Bias <brianna.bias@gmail.com>
2021-08-25 16:16:50 -0700

    emergency descriptions

--- src/components/emergencies/CrimeViolence.js
+++ src/components/emergencies/CrimeViolence.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class CrimeViolence extends Component {
       render() {
             return (
-                  <div>
-                        
-                  </div>
-            )
+              <div>
+                <h2>Crime & Violence</h2>
+                <p>
+                  Sexual and domestic assault crisis lines and shelters, child
+                  protection services, crisis lines/shelters/resources for abuse
+                  (including specified resources for LGBTQ*, disabled, elderly,
+                  minor, vulnerable adult, and immigrant populations) advocacy
+                  for support in meeting with police after an assault, safe
+                  houses, robbery/theft reporting
+                </p>
+              </div>
+            );
       }
 }
--- src/components/emergencies/Substances.js
+++ src/components/emergencies/Substances.js
@@ -4,8 +4,13 @@ export default class Substances extends Component {
       render() {
             return (
                   <div>
-                        
-                  </div>
-            )
+                        <h2>Substance Abuse</h2>
+                <p>
+                  Poison control, emergency overdose services (narcan, suboxone,
+                  etc.), substance abuse resources, clean needle share and
+                  needle disposal locations
+                </p>
+              </div>
+            );
       }
 }
